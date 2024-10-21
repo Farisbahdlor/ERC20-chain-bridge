095144766d97ae856f20c61628dfbb2d580cce6c
Farisbahdlor <39289628+Farisbahdlor@users.noreply.github.com>
2024-10-21 22:44:43 +0700

    Update IChainBridge.sol

--- interfaces/IChainBridge.sol
+++ interfaces/IChainBridge.sol
@@ -3,20 +3,19 @@ pragma solidity 0.8.27;
 
 interface IChainBridge {
 
+    function checkERC20OriginAvailable(address ERC20Token) external view returns (bool);
+    function checkERC20ForeignAvailable(address originalContractAddress, uint64 chainId) external view returns (bool);
+    function checkChainAvailable(uint64 chainID) external view returns (bool);
+    function checkERC20VestedBalance(address ERC20Token) external view returns (uint256);
     function addERC20Origin(address originalContractAddress) external returns (bool);
+    function updateERC20OriginPoT(address originalContractAddress, address PoTContractAddress) external returns (bool);
     function addERC20Foreign(address originalContractAddress, address wrappedContractAddress, uint64 chainId) external returns (bool);
     function addChain(uint64 chainID, string memory chainName) external returns (bool);
-    function sendBridge(address originalContractAddress, address receiver, uint256 numTokens, string memory message, uint64 chainDestination) external payable returns (bool);
-    function acceptBridge(bytes32 transactionHash, address sender, address receiver) external returns (bool);
-    function issueLogBridge(address sender, address receiver, address originalContractAddress, bytes32 transactionHash, uint256 numTokens, string memory message, uint64 chainId) external returns (bool);
+    function batchSendBridge(address [] memory originalContractAddress, address[] memory sender, address [] memory receiver, address [] memory smartContractSync, uint256 [] memory numTokens, string [] memory message, uint64 [] memory chainDestination) external payable returns (bool);
+    function sendBridge(address originalContractAddress, address receiver, address smartContractSync, uint256 numTokens, string memory message, uint64 chainDestination) external payable returns (bool);
+    function batchAcceptBridge(address [] memory sender, address [] memory receiver, address [] memory originalContractAddress, address[] memory smartContractSync, uint256 [] memory numTokens, string [] memory message, uint64 [] memory chainId) external returns (bool);
+    function acceptBridge(address sender, address receiver, address originalContractAddress, address smartContractSync, uint256 numTokens, string memory message, uint64 chainId) external returns (bool);
+    function redeemERC20PoT(address ownerPoT, address ERC20Token, uint256 numTokens) external returns (bool);
     function protocolERC20Transfer(address ERC20Token, address destination, uint256 numTokens) external returns (bool);
     function protocolERC20Receive(address ERC20Token, address sender, uint256 numTokens) external returns (bool);
-    
-
-    // function addERC20Origin (address originalContractAddress) external returns (bool);
-    // function addERC20Foreign (address originalContractAddress, address wrappedContractAddress, uint64 chainId) external returns (bool);
-    // function sendbridge(address originalContractAddress, uint256 numTokens, string memory message, uint64 chainDestination) external returns (bool);
-    // function acceptBridge(string memory transactionHash) external returns (bool);
-    // function issueLogBridge(address user, address originalContractAddress, string memory transactionHash, uint256 numTokens, string memory message, uint64 chainId) external returns (bool);
-    
 }
